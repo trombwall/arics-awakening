d95983ce2d9bdb56bbfdba3c7a1b15c025a73eea
Andreas Wall <andreas.wall@tromb.com>
2024-04-25 11:22:32 +0200

    Android build. Working movement.

--- scenes/player.gd
+++ scenes/player.gd
@@ -9,19 +9,24 @@ const TRAVEL_TIME := 0.3
 var tween
 
 func _physics_process(_delta):
-	if tween is Tween:
-		if tween.is_running():
-			return
+    if tween is Tween:
+        if tween.is_running():
+            return
 
-	if Input.is_action_just_pressed("forward") and not front_ray.is_colliding():
-		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-		tween.tween_property(self, "transform", transform.translated(Vector3.FORWARD * 2), TRAVEL_TIME)
-	if Input.is_action_just_pressed("backward") and not back_ray.is_colliding():
-		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-		tween.tween_property(self, "transform", transform.translated(Vector3.BACK * 2), TRAVEL_TIME)
-	if Input.is_action_just_pressed("left"):
-		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-		tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, PI / 2), TRAVEL_TIME)
-	if Input.is_action_just_pressed("right"):
-		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-		tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, -PI / 2), TRAVEL_TIME)
+    if Input.is_action_just_pressed("forward") and not front_ray.is_colliding():
+        var local_forward = transform.basis.z.normalized() # Local forward direction
+        tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+        tween.tween_property(self, "transform", transform.translated( - local_forward * 2), TRAVEL_TIME)
+
+    if Input.is_action_just_pressed("backward") and not back_ray.is_colliding():
+        var local_backward = transform.basis.z.normalized() # Local backward direction
+        tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+        tween.tween_property(self, "transform", transform.translated(local_backward * 2), TRAVEL_TIME)
+
+    if Input.is_action_just_pressed("left"):
+        tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+        tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, PI / 2), TRAVEL_TIME)
+
+    if Input.is_action_just_pressed("right"):
+        tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+        tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, -PI / 2), TRAVEL_TIME)
