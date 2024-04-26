4a6b5916d979a518d3c09eedc9519b42e44b791e
Andreas Wall <andreas.wall@tromb.com>
2024-04-26 16:15:48 +0200

    Added headbob animation

--- scenes/player.gd
+++ scenes/player.gd
@@ -10,6 +10,9 @@ const TRAVEL_TIME := 0.3
 @onready var left_ray = $LeftRay
 @onready var right_ray = $RightRay
 
+# Headbob animation
+@onready var animation = $Animation
+
 var tween
 
 func _physics_process(_delta):
@@ -22,22 +25,26 @@ func _physics_process(_delta):
         var local_forward = transform.basis.z.normalized() # Local forward direction
         tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
         tween.tween_property(self, "transform", transform.translated( - local_forward * 2), TRAVEL_TIME)
+        animation.play("headbob")
 
     if Input.is_action_just_pressed("backward") and not back_ray.is_colliding():
         var local_backward = transform.basis.z.normalized() # Local backward direction
         tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
         tween.tween_property(self, "transform", transform.translated(local_backward * 2), TRAVEL_TIME)
+        animation.play("headbob")
 
     # Left and right strafing
     if Input.is_action_just_pressed("strafe_left") and not left_ray.is_colliding():
         var local_left = transform.basis.x.normalized() # Local left direction
         tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
         tween.tween_property(self, "transform", transform.translated( - local_left * 2), TRAVEL_TIME)
+        animation.play("headbob")
 
     if Input.is_action_just_pressed("strafe_right") and not right_ray.is_colliding():
         var local_right = transform.basis.x.normalized() # Local right direction
         tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
         tween.tween_property(self, "transform", transform.translated(local_right * 2), TRAVEL_TIME)
+        animation.play("headbob")
 
     # Rotations
     if Input.is_action_just_pressed("left"):
